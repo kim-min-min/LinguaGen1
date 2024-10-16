6dd76170dd8960554de8315592f05d8f20e5c779
DevHaeng <152979415+DevHaeng@users.noreply.github.com>
2024-10-16 10:26:12 +0900

    Add files via upload
    기본 프로젝트 구성

---
+++ LinguaGen/src/test/java/com/linguagen/LinguaGenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linguagen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinguaGenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
