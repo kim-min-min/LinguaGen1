a4ceda0c4c102bf6cf47429d815321a60721239a
min <kaner0529@gmail.com>
2024-10-18 16:44:41 +0900

    dsf

--- src/main/java/com/linguagen/controller/QuestionController.java
+++ src/main/java/com/linguagen/controller/QuestionController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:5173")  // React와 CORS 문제 해결
 @RestController
