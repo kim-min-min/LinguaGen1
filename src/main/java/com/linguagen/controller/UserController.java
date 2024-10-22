62813f0e1858807502a485f4d2e8464f728ff7df
min <kaner0529@gmail.com>
2024-10-22 15:50:04 +0900

    User 클래스 수정

--- src/main/java/com/linguagen/controller/UserController.java
+++ src/main/java/com/linguagen/controller/UserController.java
@@ -1,6 +1,6 @@
 package com.linguagen.controller;
 
-import com.linguagen.entity.USERS;
+import com.linguagen.entity.User;
 import com.linguagen.service.UserService;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
@@ -25,15 +25,15 @@ public class UserController {
 
     // 모든 유저 조회
     @GetMapping
-    public ResponseEntity<List<USERS>> getAllUsers() {
-        List<USERS> users = userService.getAllUsers();
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userService.getAllUsers();
         return new ResponseEntity<>(users, HttpStatus.OK);
     }
 
     // ID로 유저 조회
     @GetMapping("/{id}")
-    public ResponseEntity<USERS> getUserById(@PathVariable String id) {
-        USERS user = userService.getUserById(id);
+    public ResponseEntity<User> getUserById(@PathVariable String id) {
+        User user = userService.getUserById(id);
         if (user != null) {
             return new ResponseEntity<>(user, HttpStatus.OK);
         } else {
@@ -43,8 +43,8 @@ public class UserController {
 
     // 유저 생성 또는 업데이트
     @PostMapping
-    public ResponseEntity<USERS> saveOrUpdateUser(@RequestBody USERS user) {
-        USERS savedUser = userService.saveOrUpdateUser(user);
+    public ResponseEntity<User> saveOrUpdateUser(@RequestBody User user) {
+        User savedUser = userService.saveOrUpdateUser(user);
         return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
     }
 
@@ -57,7 +57,7 @@ public class UserController {
     
     // 로그인
     @PostMapping("/login")
-    public String login(@RequestBody USERS user, HttpSession session) {
+    public String login(@RequestBody User user, HttpSession session) {
         boolean isAuthenticated = userService.login(user, session);
         if (isAuthenticated) {
             return "로그인 성공";
@@ -81,9 +81,9 @@ public class UserController {
         return ResponseEntity.ok("로그아웃 되었습니다.");
     }
 
-    // 등급과 경험치로 정렬된 모든 사용자 목록을 반환하는 API
-    @GetMapping("/all")
-    public List<USERS> getAllUsersWithRank() {
-        return userService.getAllUsersByRanking();
-    }
+//    // 등급과 경험치로 정렬된 모든 사용자 목록을 반환하는 API
+//    @GetMapping("/all")
+//    public List<User> getAllUsersWithRank() {
+//        return userService.getAllUsersByRanking();
+//    }
 }
--- src/main/java/com/linguagen/repository/UserRepository.java
+++ src/main/java/com/linguagen/repository/UserRepository.java
@@ -1,19 +1,18 @@
 package com.linguagen.repository;
 
-import com.linguagen.entity.USERS;
+import com.linguagen.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<USERS, String> {
+public interface UserRepository extends JpaRepository<User, String> {
     // 기본적인 CRUD 기능 제공 (findById, save 등)
     // 사용자 아이디와 비밀번호로 사용자 조회
-    Optional<USERS> findByIdAndPassword(String id, String password);
+    Optional<User> findByIdAndPassword(String id, String password);
 
     // 네이티브 쿼리를 사용하여 등급별 랭킹 조회
-    List<USERS> findAllByOrderByGradeAscExpDesc();
+//    List<User> findAllByOrderByGradeAscExpDesc();
 }
