7c54612090d7e79016bdc683b07e8ca18913d7ae
min <kaner0529@gmail.com>
2024-11-04 12:48:24 +0900

    대시보드 최근플레이

---
+++ backend/src/main/java/com/linguagen/backend/controller/DashBoardController.java
@@ -0,0 +1,29 @@
+package com.linguagen.backend.controller;
+
+import com.linguagen.backend.dto.LatestStudyInfoDto;
+import com.linguagen.backend.service.DashBoardService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
+@RequestMapping("/api/study-log")
+public class DashBoardController {
+
+    private final DashBoardService dashBoardService;
+
+    public DashBoardController(DashBoardService dashBoardService) {
+        this.dashBoardService = dashBoardService;
+    }
+
+    @GetMapping("/latest/{studentId}")  // 엔드포인트의 변수명 변경
+    public ResponseEntity<LatestStudyInfoDto> getLatestStudyInfo(@PathVariable("studentId") String studentId) {
+        LatestStudyInfoDto latestStudyInfo = dashBoardService.getLatestStudyInfo(studentId);
+        if (latestStudyInfo != null) {
+            return ResponseEntity.ok(latestStudyInfo);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    }
+}
