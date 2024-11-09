f0cd0202a7eebfd059212b2831712df9602961a0
DevHaeng <dev.haeng@gmail.com>
2024-11-09 20:45:14 +0900

    - 개인랭킹 로직 수정 및 주간 랭킹 기능 구현

--- backend/src/main/java/com/linguagen/backend/repository/RankingLogRepository.java
+++ backend/src/main/java/com/linguagen/backend/repository/RankingLogRepository.java
@@ -7,26 +7,22 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 public interface RankingLogRepository extends JpaRepository<RankingLog, Long> {
 
-    // 특정 등급의 주간 랭킹 로그를 최신순으로 조회 (grade가 0이면 전체 조회)
-    @Query("SELECT rl FROM RankingLog rl " +
-            "JOIN Grade g ON rl.user.id = g.userId " +
-            "WHERE rl.type = 'weekly' " +
-            "AND (:grade = 0 OR g.grade = :grade) " +  // grade가 0이면 전체 조회, 아니면 특정 등급 조회
-            "ORDER BY g.grade DESC, g.tier ASC, g.exp DESC, rl.logDate DESC")
-    List<RankingLog> findWeeklyRankingByGradeOrAll(int grade);
-
-    // 특정 등급의 개인 랭킹 로그를 최신순으로 조회 (grade가 0이면 전체 조회)
+    // 특정 등급의 개인 랭킹 로그를 최신순으로 조회하고, logDate가 오늘 날짜인 데이터만 필터링 (grade가 0이면 전체 조회)
     @Query("SELECT rl FROM RankingLog rl " +
             "JOIN Grade g ON rl.user.id = g.userId " +
             "WHERE rl.type = 'personal' " +
-            "AND (:grade = 0 OR g.grade = :grade) " +  // grade가 0이면 전체 조회, 아니면 특정 등급 조회
+            "AND (:grade = 0 OR g.grade = :grade) " +
+            "AND FUNCTION('DATE_FORMAT', rl.logDate, '%Y-%m-%d') = :todayDate " +
             "ORDER BY g.grade DESC, g.tier ASC, g.exp DESC, rl.logDate DESC")
-    List<RankingLog> findPersonalRankingByGradeOrAll(int grade);
+    List<RankingLog> findPersonalRankingByGradeOrAll(
+            @Param("grade") int grade,
+            @Param("todayDate") String todayDate);
 
     // 특정 날짜의 사용자와 타입에 따른 랭킹 로그 조회
     @Query("SELECT rl FROM RankingLog rl " +
